d27df0f5859a4ee3bdf218b56eb664e5d5a6c5bf
Giovanni Joubert <u18009035@tuks.co.za>
2021-05-03 11:08:58 +0200

    Enable Dev/Prod Routing

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,11 @@ const port = 3000;
 app.use(express.json());
 app.use(routes);
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
 app.listen(port, err => {
   if (err) {
     return console.error(err);
--- src/routes.ts
+++ src/routes.ts
@@ -4,14 +4,21 @@ const routes = Router();
 
 import fs from 'fs';
 const routePath = './src/modules/';
-fs.readdirSync(routePath).forEach(async function(module) {
-	if(module != '.DS_Store') {
-		const route = './src/modules/'+module+'/routes/'+module+'Routes.ts';
-		if(fs.existsSync(route)) {
-            const moduleRoutes = await import(route.replace('./src', '.'));
-            routes.use('/'+module, moduleRoutes.default);
-        }      
-	}
+fs.readdirSync(routePath).forEach(async function (module) {
+  if (module != '.DS_Store') {
+    let route;
+    if (process.env.NODE_ENV === 'development') {
+      route = './src/modules/' + module + '/routes/' + module + 'Routes.ts';
+    }
+    if (process.env.NODE_ENV === 'production') {
+      route = './dist/modules/' + module + '/routes/' + module + 'Routes.js';
+    }
+    console.log(route);
+    if (fs.existsSync(route)) {
+      const moduleRoutes = await import(route.replace('./src', '.').replace('./dist', '.'));
+      routes.use('/' + module, moduleRoutes.default);
+    }
+  }
 });
 
 export default routes;
